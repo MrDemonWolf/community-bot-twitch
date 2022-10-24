703ad4acee631749ee43824b3a5777d3f3b76876
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2022-10-23 21:55:42 -0500

    [CB-16] Updated rules modal.

--- server/modals/DiscordRule.js
+++ server/modals/DiscordRule.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const DiscordRulesSchema = new Schema({
+const DiscordRuleSchema = new Schema({
   rule: {
     type: String,
     required: true,
   },
 });
 
-module.exports = mongoose.model('DiscordRules', DiscordRulesSchema);
+module.exports = mongoose.model('DiscordRule', DiscordRuleSchema);
