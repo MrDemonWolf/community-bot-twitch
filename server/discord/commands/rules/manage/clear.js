de4e89bb8b92ce331ba39c7542c427b75bb10b8b
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2022-10-31 14:32:25 -0500

    [CB-25] Implement Rules clear updater

--- server/discord/commands/rules/manage/clear.js
+++ server/discord/commands/rules/manage/clear.js
@@ -1,8 +1,14 @@
 const consola = require('consola');
 
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} = require('discord.js');
 
 const DiscordRule = require('../../../../modals/DiscordRule');
+const DiscordGuild = require('../../../../modals/DiscordGuild');
 const { info, success } = require('../../../../utils/discord/commands/log');
 
 module.exports = async (client, interaction) => {
@@ -12,6 +18,14 @@ module.exports = async (client, interaction) => {
       interaction.user.username,
       interaction.user.id
     );
+
+    const { guild } = interaction;
+
+    const { name, id } = guild;
+
+    const discordGuild = await DiscordGuild.findOne({
+      guildId: id,
+    });
 
     const rules = await DiscordRule.find({});
 
@@ -88,6 +102,40 @@ module.exports = async (client, interaction) => {
         });
       }
     });
+
+    // edit message in rules channel
+    const rulesChannel = interaction.guild.channels.cache.get(
+      discordGuild.rules.channelId
+    );
+
+    if (!rulesChannel) {
+      consola.error({
+        message: `No rules channel found for ${interaction.guild.name}`,
+        badge: true,
+      });
+    }
+
+    const rulesMessage = await rulesChannel.messages.fetch(
+      discordGuild.rules.messageId
+    );
+
+    if (!rulesMessage) {
+      consola.error({
+        message: `No rules message found for ${interaction.guild.name}`,
+        badge: true,
+      });
+    }
+
+    const embed = new EmbedBuilder()
+      .setColor('#0099ff')
+      .setTitle(`${name} Rules`)
+      .setDescription('No rules found. Please add some first.')
+      .setTimestamp()
+      .setFooter({
+        text: 'Last updated:',
+      });
+
+    await rulesMessage.edit({ embeds: [embed] });
 
     success(
       'rules deploy clear command',
